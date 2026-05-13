2a4ec0b977f71a00b7d1b94420146c6851ad5f1e
Aariz18 <mohd.aariz13518@gmail.com>
2026-05-13 11:37:23 +0530

    Changed the backend code so that register page creates new user

--- backend/src/controllers/users.controllers.js
+++ backend/src/controllers/users.controllers.js
@@ -4,17 +4,44 @@ import jwt from 'jsonwebtoken'
 
 export const createUser = async(req, res)=>{  
     try{
+        const existingUser = await Users.findOne({
+            username:req.body.username
+        });
+
+        if(existingUser){
+            return res.status(400).json({
+                message:"Username already exists"
+            });
+        }
+
         const data = new Users({
             username:req.body.username,
             password:req.body.password
-        })
+        });
+
         const updatedData = await data.save();
-        res.status(201).json({
+
+        const token = jwt.sign(
+            {
+                username:updatedData.username,
+                room:updatedData.room
+            },
+            process.env.JWT_SECRET,
+            {expiresIn:'1h'}
+        );
+
+        return res.status(201).json({
+            token,
             username:updatedData.username,
-            password:updatedData.password
-        })
+            room:updatedData.room
+        });
+
     }catch(err){
         console.log(err);
+
+        return res.status(500).json({
+            message:"Server error"
+        });
     }
 }
 
